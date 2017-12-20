b096d9a3e7b113b63c56e5e8abc462a9f1d4133c
jonatahessa <jonata.hessa@gmail.com>
2017-12-20 13:45:20 -0200

    dom para transição de tela

--- DesenhoIndex/dom.js
+++ DesenhoIndex/dom.js
@@ -1,30 +1,83 @@
 function main() {
-  document.querySelector("#botao-sua").addEventListener("click", mostraSua);
-  document.querySelector("#botao-cardapio").addEventListener("click", mostraCardapio);
-  document.querySelector("#botao-promo").addEventListener("click", mostraPromo);
-  document.querySelector("#botao-local").addEventListener("click", mostraLocal);
-  document.querySelector("#botao-contato").addEventListener("click", mostraContato);
+  var botoes = document.querySelectorAll(".botoes-top");
+  for (var i = 0; i < botoes.length; i++) {
+    botoes[i].addEventListener("click", mudarPagina);
+  }
 }
 
-function mostraSua() {
-  document.querySelector("#botao-sua").classList.remove('selected');
-  document.querySelector("#botao-sua").classList.add('non-selected');
+function mudarPagina(event) {
+  var botao = event.target.id;
+  var botoes = document.querySelectorAll(".botoes-top");
+  desmarcaBotoes(botoes);
+  marcaBotao(botao);
+  transicaoPaginas(botao);
 }
 
-function mostraCardapio() {
-
+function marcaBotao(botao) {
+  document.getElementById(botao).classList.remove('non-selected');
+  document.getElementById(botao).classList.add('selected');
 }
 
-function mostraPromo() {
-
+function desmarcaBotoes(botoes) {
+  for (var i = 0; i < botoes.length; i++) {
+    botoes[i].classList.remove('selected');
+    botoes[i].classList.add('non-selected');
+  }
 }
 
-function mostraLocal() {
-
-}
-
-function mostraContato() {
-
+function transicaoPaginas(botao) {
+  var paginas = document.querySelectorAll(".paginas");
+  if (botao == "botao-sua") {
+    for (var i = 0; i < paginas.length; i++) {
+      if (paginas[i].id == "pagina-sua") {
+        paginas[i].classList.remove('non-show-on-click');
+        paginas[i].classList.add('show-on-click');
+      } else {
+       paginas[i].classList.add('non-show-on-click');
+       paginas[i].classList.remove('show-on-click');
+    }
+  }
+} else if (botao == "botao-cardapio") {
+    for (var i = 0; i < paginas.length; i++) {
+      if (paginas[i].id == "pagina-cardapio") {
+        paginas[i].classList.remove('non-show-on-click');
+        paginas[i].classList.add('show-on-click');
+      } else {
+        paginas[i].classList.add('non-show-on-click');
+        paginas[i].classList.remove('show-on-click');
+      }
+    }
+  } else if (botao == "botao-promo") {
+      for (var i = 0; i < paginas.length; i++) {
+        if (paginas[i].id == "pagina-promo") {
+          paginas[i].classList.remove('non-show-on-click');
+          paginas[i].classList.add('show-on-click');
+        } else {
+          paginas[i].classList.add('non-show-on-click');
+          paginas[i].classList.remove('show-on-click');
+        }
+      }
+    } else if (botao == "botao-local") {
+        for (var i = 0; i < paginas.length; i++) {
+          if (paginas[i].id == "pagina-local") {
+            paginas[i].classList.remove('non-show-on-click');
+            paginas[i].classList.add('show-on-click');
+          } else {
+            paginas[i].classList.add('non-show-on-click');
+            paginas[i].classList.remove('show-on-click');
+          }
+        }
+      } else if (botao == "botao-contato") {
+          for (var i = 0; i < paginas.length; i++) {
+            if (paginas[i].id == "pagina-contato") {
+              paginas[i].classList.remove('non-show-on-click');
+              paginas[i].classList.add('show-on-click');
+            } else {
+              paginas[i].classList.add('non-show-on-click');
+              paginas[i].classList.remove('show-on-click');
+            }
+        }
+    }
 }
 
 window.addEventListener("load", main);
